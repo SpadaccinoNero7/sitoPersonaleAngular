0c1ddc42c1c4d40742b39239d13adddfdee5a446
SpadaccinoNero7 <mattiaserafini0@gmail.com>
2025-11-11 15:26:02 +0100

    feat(datepicker): added

--- src/components/homePage/homePage.component.ts
+++ src/components/homePage/homePage.component.ts
@@ -1,9 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-homePage',
-  imports: [],
+  imports: [MatButtonModule, MatDividerModule, MatIconModule],
   templateUrl: './homePage.component.html',
   styleUrl: './homePage.component.scss',
 })
--- src/components/userList/userList.component.ts
+++ src/components/userList/userList.component.ts
@@ -1,9 +1,22 @@
 import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { Router } from '@angular/router';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-userList',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatDatepickerModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './userList.component.html',
   styleUrl: './userList.component.scss',
 })
