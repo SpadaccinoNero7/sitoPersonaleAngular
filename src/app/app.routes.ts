6e7017a5443f04d32720d42450e9712bc0df850b
SpadaccinoNero7 <mattiaserafini0@gmail.com>
2025-11-11 09:35:17 +0100

    first commit

---
+++ src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from '../components/homePage/homePage.component';
+
+export const routes: Routes = [{ path: '', component: HomeComponent }];
