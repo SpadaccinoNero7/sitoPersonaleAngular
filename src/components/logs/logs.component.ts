ce00e7fe7db1acc8be148fb8068b514f283bbbcf
SpadaccinoNero7 <mattiaserafini0@gmail.com>
2025-11-11 16:16:51 +0100

    feat(logs): added date and select

--- src/components/logs/logs.component.ts
+++ src/components/logs/logs.component.ts
@@ -3,10 +3,22 @@ import { Router } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-logs',
-  imports: [MatButtonModule, MatDividerModule, MatIconModule],
+  imports: [
+    MatButtonModule,
+    MatDividerModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatDatepickerModule,
+  ],
   templateUrl: './logs.component.html',
   styleUrl: './logs.component.scss',
 })
